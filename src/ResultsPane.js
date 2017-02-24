c2ea3e3bd5b3403b77001196605a48db23a33906
Suhair Zain <suhairzain11@gmail.com>
2017-02-24 15:37:31 +0530

    Created ResultsPane

---
+++ src/ResultsPane.js
@@ -0,0 +1,72 @@
+/* eslint-env commonjs */
+/* eslint-disable require-jsdoc */
+
+import React, {
+    Component,
+    PropTypes,
+} from 'react';
+
+const BarChart = require('react-chartjs').Bar;
+
+const styles = {
+    root: {
+        alignItems: 'flex-end',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    chart: {
+        height: '100%',
+        width: '100%',
+    },
+};
+
+const getData = (correct, incorrect) => ({
+    labels: ['Correct', 'Incorrect'],
+    datasets: [{
+        label: 'SCORE',
+        data: [correct, incorrect],
+        backgroundColor: [
+            '#000',
+            '#000',
+        ],
+        borderColor: [
+            '#000',
+            '#f00',
+        ],
+        borderWidth: 10,
+    }],
+});
+
+const options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                max: 4,
+                min: 0,
+                stepSize: 0.5,
+            },
+        }],
+    },
+};
+
+class ResultsPane extends Component {
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="results-pane" style={styles.root}>
+                <BarChart
+                    data={getData(this.props.correct, this.props.incorrect)}
+                    options={options} />
+            </div>
+        );
+    }
+}
+
+ResultsPane.propTypes = {
+    correct: PropTypes.number.isRequired,
+    incorrect: PropTypes.number.isRequired,
+};
+
+export default ResultsPane;
