c7c6648a87e1f44aba50c04e5f3afe55f586da9a
Suhair Zain <suhairzain11@gmail.com>
2017-02-24 18:16:44 +0530

    Updated questions to match those in mail

--- src/data.js
+++ src/data.js
@@ -1,20 +1,44 @@
+/* eslint-disable max-len */
+
 export default [
     {
-        question: 'When was the last time India won the Cricket World Cup?',
+        question: 'When was the last time India won the Cricket World Cup ?',
         options: [
-            '2008',
-            '2012',
-            '2016',
+            '2003',
+            '2007',
+            '2011',
+            '2015',
         ],
-        correctAnswer: '2012',
+        correctAnswer: '2011',
     },
     {
-        question: 'Who has won the most number of Cricket World Cups?',
+        question: 'What is the highest individual score by a batsman in Test Cricket ?',
+        options: [
+            '376',
+            '400',
+            '403',
+            '430',
+        ],
+        correctAnswer: '400',
+    },
+    {
+        question: 'Who has won the most number of Cricket World Cups ?',
         options: [
             'Australia',
             'India',
             'England',
+            'West Indies',
         ],
         correctAnswer: 'Australia',
+    },
+    {
+        question: 'How many International centuries does Sachin Tendulkar have under his name ?',
+        options: [
+            '80',
+            '86',
+            '97',
+            '100',
+        ],
+        correctAnswer: '100',
     },
 ];
